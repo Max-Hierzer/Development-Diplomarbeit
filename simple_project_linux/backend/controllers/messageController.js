1ab74bd6c7494d1c6ad9ac0b605550918dbc3f46
Max-Hierzer <max.m.hierzer@gmail.com>
2024-11-06 20:42:47 +0100

    message controller

---
+++ simple_project_linux/backend/controllers/messageController.js
@@ -0,0 +1,19 @@
+const { createMessage } = require('../services/messageServices');
+
+async function handleCreateMessage(req, res) {
+    try {
+        const { text } = req.body;
+        if (!text) {
+            return res.status(400).json({ error: 'Message text is required' });
+        }
+        const newMessage = await createMessage(text);
+        res.status(201).json(newMessage);
+    } catch (error) {
+        console.error('Error inserting message:', error);
+        res.status(500).json({error: 'Error creating message in controller' });
+    }
+}
+
+module.exports = {
+    handleCreateMessage: handleCreateMessage
+}
