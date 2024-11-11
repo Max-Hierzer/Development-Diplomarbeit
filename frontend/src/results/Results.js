0dda1a732d210672f2100b6a53f45cd98ef9a94d
Max-Hierzer <max.m.hierzer@gmail.com>
2024-11-11 17:41:50 +0100

    disclose who voted for which option

--- frontend/src/results/Results.js
+++ frontend/src/results/Results.js
@@ -3,7 +3,8 @@ import React, { useState, useEffect } from 'react';
 const Results = () => {
     const [results, setResults] = useState([]);
     const [polls, setPolls] = useState([]);
-
+    const [users, setUsers] = useState([]);
+    const [showVotersMode, setShowVoters] = useState(true);
     useEffect(() => {
         const fetchResults = async () => {
             try {
@@ -32,15 +33,34 @@ const Results = () => {
         fetchPolls();
     }, []);
 
-    const countResults = (results, question, answer) => {
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const res = await fetch('http://localhost:3001/api/users');
+                const data = await res.json();
+                setUsers(data);
+            } catch (error) {
+                console.error('Error fetching users:', error);
+            }
+        };
+        fetchUsers();
+    }, []);
+
+    const showResults = (results, question, answer) => {
         let counter = 0;
+        let voters = [];
         results.map((r) => {
-            if (r.questionId === question.id && r.answerId === answer.id) counter++;
+            if (r.questionId === question.id && r.answerId === answer.id) {
+                counter++;
+                voters.push(r.userId);
+            }
+            return 0;
         });
-        return counter;
+        return { counter, voters };
     }
 
-    console.log(polls);
+    const showVoters = () => { setShowVoters(!showVotersMode)};
+
     return (
         <div>
         <h1>Polls and Questions</h1>
@@ -52,13 +72,18 @@ const Results = () => {
                 <h4>{question.name}</h4>
                 {question.Answers && question.Answers.map((answer) => (
                     <div key={answer.id} className="answer">
-                    <p>{answer.name} {countResults(results, question, answer)}</p>
+                    <p>{answer.name} {showVotersMode ?
+                        showResults(results, question, answer).counter :
+                        users.filter(u => showResults(results, question, answer).voters.includes(u.id)).map(u => u.name + ', ')
+                    }</p>
+
                     </div>
                 ))}
                 </div>
             ))}
             </div>
         ))}
+        <button onClick={showVoters}>{ showVotersMode ? ('Show voters') : ('Show count')}</button>
         </div>
     );
 }
