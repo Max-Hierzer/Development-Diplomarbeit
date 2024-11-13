de84313eadb5abfa28fb247b7debd090209178bb
Max-Hierzer <max.m.hierzer@gmail.com>
2024-11-13 18:33:11 +0100

    changed so server doesnt stop because multiple votes on same question

--- backend/services/votingServices.js
+++ backend/services/votingServices.js
@@ -2,12 +2,28 @@
 const { UserAnswers, Users, Answers, Questions } = require('../models/index');
 
 async function submitVote(userId, answers) {
-    // Insert answers into the `UserAnswers` table
-    const userAnswersPromises = Object.entries(answers).map(([questionId, answerId]) =>
-    UserAnswers.create({ userId, answerId, questionId })
-    );
+    try {
+        for (const [questionId, answerId] of Object.entries(answers)) {
+            const existingVote = await UserAnswers.findOne({
+                where: {
+                    userId: userId,
+                    questionId: questionId,
+                    answerId: answerId,
+                },
+            });
 
-    await Promise.all(userAnswersPromises);
+            if (existingVote) {
+                throw new Error(`User has already voted for question ${questionId} with answer ${answerId}`);
+            }
+        }
+        const userAnswers = Object.entries(answers).map(([questionId, answerId]) =>
+        UserAnswers.create({ userId, answerId, questionId })
+        );
+        return userAnswers;
+    } catch (error) {
+        console.error('Error creating vote in service:', error);
+        throw error;
+    }
 }
 
 module.exports = { submitVote };
