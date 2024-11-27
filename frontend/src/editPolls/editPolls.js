81ecd838fd6c36cbe98705c4f5083a440d4922da
Max Hierzer <max.hierzer@bulme.at>
2024-11-27 12:09:07 +0100

    .

--- frontend/src/editPolls/editPolls.js
+++ frontend/src/editPolls/editPolls.js
@@ -2,25 +2,25 @@
 
 function EditPolls({selectedPoll, polls}) {
     const poll = polls.filter((poll) => poll.id.toString() === selectedPoll)[0];
-    function Hallo(event){
+    function handleSubmit(event){
         event.preventDefault();
         console.log("Hallo")
     }
     return (
         <div>
         { selectedPoll ? (
-        <form onSubmit={(event) => Hallo(event)}>
+        <form onSubmit={(event) => handleSubmit(event)}>
         <h1>Pollname</h1>
-        <input value={poll.name}/>
+        <input defaultValue={poll.name}/>
         {poll.Questions && poll.Questions.map((question) => (
             <div key={question.id} className="Question">
             <h2>Question</h2>
-            <input value ={question.name} />
+            <input defaultValue={question.name} />
             <h3>Answers</h3>
             {
                 question.Answers && question.Answers.map((answer) => (
                     <div key={answer.id} className="Answer">
-                    <input value={answer.name} />
+                    <input defaultValue={answer.name} />
                     </div>
                 ))
             }
