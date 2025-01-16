2be6e15574092592aecdb7a981e46a3ac28e2758
Tobias <tobias.prasser@gmail.com>
2025-01-16 14:05:30 +0100

    select Poll bug fixed

--- frontend/src/selectPolls/SelectPolls.js
+++ frontend/src/selectPolls/SelectPolls.js
@@ -5,8 +5,10 @@ const SelectPolls = ({ polls, handleSetSelectedPoll, selectedPoll }) => {
     return (
         <div>
         <h2>Select a Poll</h2>
-        <select onChange={(e) => handleSetSelectedPoll(e.target.value)}>
-        <option defaultValue="">Select a poll</option>
+        <select
+            value={selectedPoll ? selectedPoll.id : ""}
+            onChange={(e) => handleSetSelectedPoll(e.target.value)}>
+        <option value="">Select a poll</option>
         {polls.map((poll) => (
             <option key={poll.id} value={poll.id}>
             {poll.name}
