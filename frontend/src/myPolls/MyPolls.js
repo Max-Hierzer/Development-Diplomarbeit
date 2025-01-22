9f73def0db723f445cc494fa35f28c805272c5b3
Tobias <tobias.prasser@gmail.com>
2025-01-22 12:53:08 +0100

    linktext in mypoll deleted and button shows clicked if clicked

--- frontend/src/myPolls/MyPolls.js
+++ frontend/src/myPolls/MyPolls.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 function MyPoll({ pollId, isPublic, isAnonymous}) {
     const [response, setResponse] = useState(null);
+    const [copiedText, setCopiedText] = useState(null);
 
     const voteHash = encodeURIComponent(btoa(`public=${isPublic}&mode=vote&poll=${pollId}&anonymous=${isAnonymous}`));
     const resultsHash = encodeURIComponent(btoa(`public=${isPublic}&mode=results&poll=${pollId}&anonymous=${isAnonymous}`));
@@ -14,19 +15,30 @@ function MyPoll({ pollId, isPublic, isAnonymous}) {
         event.preventDefault();
     };
 
+    const copyClipboard = async(type) => {
+        if (type) {
+            await navigator.clipboard.writeText(voteLink);
+        } else {
+            await navigator.clipboard.writeText(resultsLink);
+        }
+
+        setCopiedText(type);
+        setTimeout(() => setCopiedText(null), 1000);
+    }
+
 
     return (
         <div>
             <h3>Vote Link:</h3>
-            <br />
-            <h5>{voteLink}</h5>
-            <button onClick={() => navigator.clipboard.writeText(voteLink)}>Copy</button>
+            <button onClick={() => copyClipboard(1)}>
+                {copiedText === 1 ? "Copied!" : "Copy Link"}
+            </button>
             <br />
             <br />
             <h3>Results Link:</h3>
-            <br />
-            <h5>{resultsLink}</h5>
-            <button onClick={() => navigator.clipboard.writeText(resultsLink)}>Copy</button>
+            <button onClick={() => copyClipboard(0)}>
+                {copiedText === 0 ? "Copied!" : "Copy Link"}
+            </button>
             <br />
             <p>{response}</p>
             <br />
