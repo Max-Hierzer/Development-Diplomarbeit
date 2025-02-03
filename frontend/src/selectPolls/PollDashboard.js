b042933273136480b488ec615f1ed66970e7fae2
gernotfasching <gernotfasch@gmail.com>
2025-02-03 19:54:54 +0100

    submitVote now works with warning

--- frontend/src/selectPolls/PollDashboard.js
+++ frontend/src/selectPolls/PollDashboard.js
@@ -181,7 +181,7 @@ const PollDashboard = ({ userId, userName, userRoleId }) => {
     const handleAnswerChange = (questionId, answerId, isMultipleChoice = false, checked = false) => {
         setSelectedAnswers((prevAnswers) => {
             if (isMultipleChoice) {
-                const currentAnswers = prevAnswers[questionId]?.answers || [];
+                const currentAnswers = prevAnswers[questionId]?.answer || [];
                 return {
                     ...prevAnswers,
                     [questionId]: {
