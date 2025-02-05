15bc3c6dcca1602c2ef41e23fde8c04464edca6d
Tobias <tobias.prasser@gmail.com>
2025-02-05 17:44:21 +0100

    styled and bugfixes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,15 @@
 // routes/userRoutes.js
 const express = require('express');
-const { handleCreateUser, handleFetchLogin } = require('../controllers/userController');
+const { handleCreateUser, handleFetchUsers, handleFetchLogin } = require('../controllers/userController');
 
 const router = express.Router();
 
 // give api names
 const postUser = router.post('/user', handleCreateUser);
+const getUsers = router.get('/users', handleFetchUsers);
 const getLogin = router.post('/login', handleFetchLogin);
 module.exports = {
     postUser: postUser,
+    getUsers: getUsers,
     getLogin: getLogin
 };
