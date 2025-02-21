1651238df5f6fea0dc494fb9d86f897d4ff1e685
Tobias <tobias.prasser@gmail.com>
2025-02-21 19:39:17 +0100

    changed footer to like like on the lmp site

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,9 +5,11 @@ import './Footer.css';
 function Footer(){
     return (
         <footer className="footer">
-            <a>&copy; {new Date().getFullYear()}</a>
-            <a className="link_text"  href="https://liste-petrovic.at/impressum/">Impressum</a>
-            <a className="link_text"  href="https://liste-petrovic.at/datenschutzerklaerung/">Datenschuterklärung</a>
+            <p className="footer-copyright">&copy; {new Date().getFullYear()}</p>
+            <div className="footer-links">
+                <a className="link_text"  href="https://liste-petrovic.at/impressum/">Impressum</a>
+                <a className="link_text"  href="https://liste-petrovic.at/datenschutzerklaerung/">Datenschuterklärung</a>
+            </div>
         </footer>
     );
 }
